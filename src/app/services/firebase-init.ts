1e3207df4cd907f39bb294047fd2a3330ba2603b
noaGJ <noa@gravityjack.com>
2025-06-24 18:29:11 -0600

    Initialize Firebase.

---
+++ src/app/services/firebase-init.ts
@@ -0,0 +1,14 @@
+﻿import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "MEASUREMENT_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
