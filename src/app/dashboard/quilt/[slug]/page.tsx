6c3c14e51e0b8173ebd11382bfea89988313c901
noaGJ <noa@gravityjack.com>
2025-08-27 13:46:08 -0600

    Update quilt slug with params.

--- src/app/dashboard/quilt/[slug]/page.tsx
+++ src/app/dashboard/quilt/[slug]/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
  * Displays the patches contained in a specific quilt.
  * @constructor
  */
-export default function Quilt() {
+export default function Quilt({params}: { params: Promise<{ slug: string }>}) {
     return (
         <>
             <h1>Quilt</h1>
